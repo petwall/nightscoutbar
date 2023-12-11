302038cd631301622b80301fc34f92ac52516c18
Peter Wallman <peter.wallman@klarna.com>
2023-12-11 16:22:35 +0100

    Initial Commit

---
+++ nightscoutbar/nightscoutbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  nightscoutbarApp.swift
+//  nightscoutbar
+//
+//  Created by Peter Wallman on 2023-12-11.
+//
+
+import SwiftUI
+
+@main
+struct nightscoutbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
